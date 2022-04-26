dd5a38ab3866d65ea76173c93b4518a2894eb685
nokoe <10518515-nokoe@users.noreply.gitlab.com>
2022-04-26 21:40:24 +0200

    drag and drop mods

--- scripts/menu_mods.gd
+++ scripts/menu_mods.gd
@@ -6,17 +6,53 @@ const MOD_LIST_ITEM_SCENE = preload("res://scenes/ModListItem.tscn")
 
 var next_mod = ""
 var next_label = ""
+var current_thread: Thread
 
 onready var list_node = $HBoxContainer/VBoxContainer/HBoxContainer/VBoxContainer/list_blur/list_darken/ScrollContainer/mods_list
+onready var loading = $HBoxContainer/VBoxContainer/HBoxContainer/VBoxContainer/list_blur/list_darken/loading
 
 
 func _ready():
-	pass
+	get_tree().connect("files_dropped", self, "_on_files_dropped")
+
+
+func _process(delta):
+	if loading.visible:
+		loading.get_node("spinner_anchor/Sprite").rotation_degrees += delta * get_parent().LOADING_SPINNER_SPEED
+
+
+func _on_files_dropped(files, _screen):
+	if visible:
+		if current_thread == null:
+			current_thread = Thread.new()
+		else:
+			current_thread.wait_to_finish()
+
+		current_thread.start(self, "copy_mod", files)
+
+
+func copy_mod(files):
+	var d = Directory.new()
+	call_deferred("start_loading")
+	for i in files:
+		if i.get_extension() == "pck":
+			d.copy(i, Mods.MODS_PATH + "/" + i.get_file())
+	call_deferred("stop_loading")
+	return
+
+
+func start_loading():
+	hide_mods()
+	loading.show()
+
+
+func stop_loading():
+	show_mods()
+	loading.hide()
 
 
 func show_mods():
-	for i in list_node.get_children():
-		i.queue_free()
+	hide_mods()
 	
 	var inactive = Mods.get_inactive()
 	var active = Mods.get_active()
@@ -39,6 +75,12 @@ func show_mods():
 				list_node.add_child(mod_item_instance)
 	else:
 		$HBoxContainer/VBoxContainer/HBoxContainer/VBoxContainer/list_blur/list_darken/no_mods.show()
+
+
+func hide_mods():
+	$HBoxContainer/VBoxContainer/HBoxContainer/VBoxContainer/list_blur/list_darken/no_mods.hide()
+	for i in list_node.get_children():
+		i.queue_free()
 
 
 func toggle_mod(file_name):
@@ -66,3 +108,6 @@ func _on_BackMods_pressed():
 
 func _on_no_mods_pressed():
 	OS.shell_open(ProjectSettings.globalize_path("user://mods"))
+
+func _exit_tree():
+	current_thread.wait_to_finish()
