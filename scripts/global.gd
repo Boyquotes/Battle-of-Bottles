e3395e6db56140faa5fa54d6c623b541fdd48c20
tpart <10518520-tpart@users.noreply.gitlab.com>
2022-07-15 15:38:41 +0200

    rename mp_client.gd to multiplayer.gd

--- scripts/global.gd
+++ scripts/global.gd
@@ -20,10 +20,10 @@ const customizations = {
 		"sunglasses": ["Sunglasses", preload("res://assets/customizations/Sunglasses.tscn")]
 	},
 	"bottles": {
-		"default_bottle": ["Default", "What have I done to you?", preload("res://assets/customizations/DefaultBottle.tscn"), preload("res://assets/customizations/DefaultBottle_broken.tscn")],
-		"ketchup_bottle": ["\"Ketchup\"", "It's ketchup, nothing else", preload("res://assets/customizations/KetchupBottle.tscn"), preload("res://assets/customizations/KetchupBottle_broken.tscn")],
+		"default_bottle": ["Default", "Boooring!", preload("res://assets/customizations/DefaultBottle.tscn"), preload("res://assets/customizations/DefaultBottle_broken.tscn")],
+		"ketchup_bottle": ["\"Ketchup\"", "It's ketchup, nothing else.", preload("res://assets/customizations/KetchupBottle.tscn"), preload("res://assets/customizations/KetchupBottle_broken.tscn")],
 		"superbottle": ["Superbottle", "Is it a bird? Is it a plane? No, it's Superbottle!", preload("res://assets/customizations/SuperBottle.tscn"), preload("res://assets/customizations/SuperBottle_broken.tscn")],
-		"cyberbottle": ["CYBERBOTTLE", "(almost) unbreakable", preload("res://assets/customizations/CyberBottle.tscn"), preload("res://assets/customizations/CyberBottle_broken.tscn")],
+		"cyberbottle": ["CYBERBOTTLE", "(almost) unbreakable.", preload("res://assets/customizations/CyberBottle.tscn"), preload("res://assets/customizations/CyberBottle_broken.tscn")],
 		"suitbottle": ["Suit Bottle", "Nothin' suits me like a suit!", preload("res://assets/customizations/SuitBottle.tscn"), preload("res://assets/customizations/SuitBottle_broken.tscn")],
 	}
 }
